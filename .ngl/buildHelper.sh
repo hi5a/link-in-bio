724f2e9efa3ae44267866d7ae6ca022396d1ee75
Andrew <okbomer999@gmail.com>
2026-03-04 21:18:54 -0500

    add basic.html

--- .ngl/buildHelper.sh
+++ .ngl/buildHelper.sh
@@ -1,6 +1,8 @@
+# SPDX-FileCopyrightText: Andrew Ball <andrew@aball.dev>
 npm run trueBuild
 SHORT_COMMIT=$(git rev-parse --short HEAD)
 LONG_COMMIT=$(git rev-parse HEAD)
+echo "Building project from $SHORT_COMMIT"
 
 sed -i "s/{{SHORT_COMMIT}}/$SHORT_COMMIT/g" ./dist/index.html
 sed -i "s/{{LONG_COMMIT}}/$LONG_COMMIT/g" ./dist/index.html
